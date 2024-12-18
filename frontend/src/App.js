268e7ddf10cff24c364acf41c3054000de98137c
asjadahamed77 <asjadahamed77@gmail.com>
2024-12-18 09:06:09 +0530

    Implemented React Toastify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import SignUp from './pages/SignUp';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className=' '>
       <BrowserRouter>
+      <ToastContainer />
       <Navbar />
       <Routes>
         <Route path='sign-up' element={<SignUp />} />
